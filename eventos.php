e26ee6fa6d944eb85ee2a7e10c3576382b55fefd
raulBrady <bradyraule@gmail.com>
2021-01-22 10:03:04 -0500

    agendado eventos...

---
+++ eventos.php
@@ -0,0 +1,14 @@
+<?php
+    header('Content-Type: application/json');
+    $pdo= new PDO("mysql:dbname=sistema;host=127.0.0.1","root","");
+
+    //Seleccionar los eventos del calendario
+
+    $sentenciaSQL = $pdo->prepare("SELECT * FROM eventos");
+    $sentenciaSQL->execute();
+
+    $resultado= $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($resultado);
+
+
+?>
